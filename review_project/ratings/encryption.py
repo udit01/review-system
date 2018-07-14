2afff83e4d0f87d3bc5b7fac5339bba4d4484073
Divyansh <divi09112@gmail.com>
2018-07-14 20:05:42 +0530

    encrypt-decrypt methods added

---
+++ review_project/ratings/encryption.py
@@ -0,0 +1,30 @@
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from random import random
+
+global master_key
+
+#randfunc for RSA keypair generation
+def my_rand(n):
+    my_rand.counter += 1
+
+    return PBKDF2(master_key, b"my_rand:%d" % my_rand.counter, dkLen=n, count=1)
+
+#returns an RSA key object
+def generate_key(password, salt):
+	print(type(password), type(salt))
+	global master_key
+	master_key = PBKDF2(password, salt, count=10000)
+
+	my_rand.counter = 0
+	RSA_key = RSA.generate(2048, randfunc=my_rand)
+	
+	return RSA_key
+
+def encrypt(plaintext, key, k = random()):  #key is public key
+	return key.encrypt(plaintext.encode("utf-8"),k)
+
+def decrypt (ciphertext, key):
+	return key.decrypt(ciphertext)
+
